6a4177696939e167f63c2f80f945685b35222cf3
Fahad Jawed <Fahadjawedmolai03@gmail.com>
2025-02-04 23:02:11 +0100

    Add files via upload

---
+++ Bubble sort/Bubble sort.py
@@ -0,0 +1,19 @@
+def recursive_bubble_sort(A,n=None):
+    if n is None:
+        n = len(A)
+    
+    elif n == 1:
+        return
+    
+    for j in range(n-1):
+        if A[j] > A[j+1]:
+            A[j],A[j+1] = A[j+1],A[j]        
+    
+    recursive_bubble_sort(A, n-1)
+    
+
+A = [2, 3, 1, 0, 5]
+recursive_bubble_sort(A)
+print(A)
+    
+
